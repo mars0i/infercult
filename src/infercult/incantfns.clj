9e879a457e1d6d314b12b4c2f7ee7d249ffb9063
mars0i <mabrams@uab.edu>
2016-03-26 14:26:58 -0500

    added freq-pairs-plot

--- src/infercult/incantfns.clj
+++ src/infercult/incantfns.clj
@@ -5,12 +5,25 @@
 ;; file LICENSE, except where noted.
 
 (ns infercult.incantfns
-  (:require [incanter.charts :as ch]))
+  (:require [incanter.charts :as ch]
+            [infercult.models :as im]))
 
 (defn xyp
-  [xs]
-  (ch/xy-plot (range) xs))
+  "Calls incanter.charts/xy-plot with y coordinates ys, adding x coordinates
+  from zero to n as needed."
+  [ys]
+  (ch/xy-plot (range) ys))
 
 (defn add
+  "Calls incanter.charts/add-lines with y coordinates ys, adding x coordinates
+  from zero to n as needed."
   [p xs]
   (ch/add-lines p (range) xs))
+
+(defn freq-pairs-plot
+  [p q r s x y n]
+  (let [pairs (take n (im/freq-pairs p q r s x y))
+        plt (xyp (map first pairs))]
+    (add plt (map second pairs))
+    (ch/set-y-range plt 0.0 1.0)))
+
