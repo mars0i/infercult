9aee5241f0aced55ece53ababb88d65f9c8d6579
mars0i <mabrams@uab.edu>
2016-03-25 23:24:40 -0500

    recorded obsolete function def

--- src/infercult/oldcode.clj
+++ src/infercult/oldcode.clj
@@ -19,3 +19,20 @@
         rqy (* infer-prob other-transm-prob other-freq)]
     (- (+ px rqy) 
        (* px rqy))))
+
+
+;; FIXME DOESN'T WORK
+;; Intended to allow iterating next-freq-2
+;; e.g. like this:
+;; (double-cross-iterate (partial next-freq-2 0.5 0.2 0.8) (partial next-freq-2 0.5 0.8 0.2)  [0.5 0.4]))
+(defn double-cross-iterate
+  "Returns a lazy sequence of
+     [x y], [(f x y) (g y x)], 
+     [(f (f x y) (g y x))
+      (g (g y x) (f x y))], etc.
+  i.e. at each step, takes the result [x y] from the previous step and 
+  generates [x' y'] by applying f to x and y, and g to the same values 
+  in reverse order."
+  [f g [x y]]
+  (cons [x y]
+        (lazy-seq double-cross-iterate f g [(f x y) (g y x)])))
